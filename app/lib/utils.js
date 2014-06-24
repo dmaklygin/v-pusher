0eea0ff2c1b93fccb6efbaabdf04b35cb60db032
Dmitry Maklygin <d.maklygin@gmail.com>
2014-06-24 19:39:12 +0400

    Sugar

---
+++ app/lib/utils.js
@@ -0,0 +1,17 @@
+/**
+ * Created by dmitrymaklygin on 24.06.14.
+ */
+
+var utils = exports;
+
+
+utils.extend = function(dest, source, overwrite) {
+	if (!source) return dest;
+	for (var key in source) {
+		if (!source.hasOwnProperty(key)) continue;
+		if (dest.hasOwnProperty(key) && overwrite === false) continue;
+		if (dest[key] !== source[key])
+			dest[key] = source[key];
+	}
+	return dest;
+};
